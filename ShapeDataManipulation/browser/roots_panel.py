735f7ffcafcf17a4587cee9cb59437247b5265ee
piotr_sikora <piotr_sikora@localhost>
2012-09-06 22:47:28 +0200

    ui/performance fix: scrolling roots of shape hierarchies - now with buttons

--- ShapeDataManipulation/browser/roots_panel.py
+++ ShapeDataManipulation/browser/roots_panel.py
@@ -17,14 +17,15 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+
 import wx
 import wx.lib.scrolledpanel
 from internal.image_conversion import PilImageToWxBitmap
 
 class RootPanel(wx.Panel):
-    def __init__(self, shape, *args, **kwargs):
+    def __init__(self, shape, main_widget, *args, **kwargs):
         wx.Panel.__init__(self, *args, **kwargs)
-        self.parent = kwargs['parent']
+        self.roots_widget = main_widget
         self.shape = shape
         sizer = wx.BoxSizer(wx.VERTICAL)
         imagePanel = wx.Panel(self)
@@ -49,6 +50,7 @@ class RootPanel(wx.Panel):
         self.Bind(wx.EVT_LEFT_DOWN, self.OnChooseThisShape)
         shapeImage.Bind(wx.EVT_LEFT_DOWN, self.OnChooseThisShape)
         imagePanel.Bind(wx.EVT_LEFT_DOWN, self.OnChooseThisShape)
+        self.width = max(info.GetSize()[0],imagePanel.GetSize()[0]) + 10 
         
     def OnChooseThisShape(self, event):
         self.select()
@@ -58,43 +60,24 @@ class RootPanel(wx.Panel):
         
     def select(self):
         self.SetBackgroundColour('#00ff00')
-        self.parent.data.current_hierarchy = self.shape
-        self.parent.data.current_shape = self.shape
-        if self.parent.currently_selected_subpanel is not None:
-            self.parent.currently_selected_subpanel.deselect()
-        self.parent.currently_selected_subpanel = self 
-        for panel in self.parent.target_panels:
+        self.roots_widget.data.current_hierarchy = self.shape
+        self.roots_widget.data.current_shape = self.shape
+        if self.roots_widget.currently_selected_subpanel is not None:
+            self.roots_widget.currently_selected_subpanel.deselect()
+        self.roots_widget.currently_selected_subpanel = self 
+        for panel in self.roots_widget.target_panels:
             panel.regenerate()
-        
-class RootsPanel(wx.lib.scrolledpanel.ScrolledPanel):
+       
+#class RootsPanel(wx.lib.scrolledpanel.ScrolledPanel):
+class RootsPanel(wx.Panel):
     
     def __init__(self, data, target_panels, sorting_method, *args, **kwargs):
-        wx.lib.scrolledpanel.ScrolledPanel.__init__(self, *args, **kwargs)
+        #wx.lib.scrolledpanel.ScrolledPanel.__init__(self, *args, **kwargs)
+        wx.Panel.__init__(self, *args, **kwargs)
         self.data = data
         self.target_panels = target_panels
         self.sorting_method = sorting_method
         self.root_panels = []
-        self.prepare_for_new_layout()
-        
-    def regenerate(self):
-        sizer = self.prepare_for_new_layout()
-        for panel in self.root_panels:
-            panel.Destroy()
-        self.root_panels = []
-        for hierarchy_root in self.data.sorted_hierarchies(self.sorting_method):
-            panel = RootPanel(parent = self, shape = hierarchy_root)
-            self.root_panels.append(panel)
-            sizer.Add(panel, 0, wx.ALL | wx.ALIGN_TOP, 5)
-            sizer.AddSpacer(5)
-        sizer.AddStretchSpacer()
-        self.SetSizer(sizer, True)
-        #self.SetupScrolling(scroll_y = False)
-        self.SetupScrolling()
-        self.Refresh()
-        self.Update()
-    
-    def prepare_for_new_layout(self):
-        self.currently_selected_subpanel = None
         static_label = 'Korzenie hierarchii kształtów'
         if self.sorting_method is None:
             static_label += " (nieposortowane)"
@@ -104,8 +87,96 @@ class RootsPanel(wx.lib.scrolledpanel.ScrolledPanel):
             static_label += " od najliczniejszej hierarchii do najmniej licznych"
         staticbox = wx.StaticBox(self, label = static_label)
         sizer = wx.StaticBoxSizer(staticbox, orient = wx.HORIZONTAL)
+
+        self.buttons = {}
+        self._bindings = {'next' : self.next_root,
+                          'nextscr': self.next_scroll_roots,
+                          'end': self.last_root,
+                          'prev': self.prev_root,
+                          'prevscr': self.prev_scroll_roots,
+                          'home': self.first_root}
+        self._button_tooltips = {'next' : u'Pokaż następną hierarchię.',
+                          'nextscr': u'Przewiń hierarchie tak, by pierwszą widoczną była następna po obecnie wyświetlanych.',
+                          'end': u'Pokaż ostatnią hierarchię.',
+                          'prev': u'Pokaż poprzednią hierarchię.',
+                          'prevscr': u'Przewiń hierarchie tak, by ostatnią widoczną była poprzednia przed obecnie wyświetlanymi.',
+                          'home': u'Pokaż pierwszą hierarchię.'}
+        for button_name in ['next', 'nextscr', 'end', 'prev', 'prevscr', 'home']:
+            image = wx.Image('resource/' + button_name + '.png', wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+            self.buttons[button_name] = wx.BitmapButton(self, bitmap = image,
+                             size = (image.GetWidth()+10, image.GetHeight()))
+            tooltip = wx.ToolTip(self._button_tooltips[button_name])
+            self.buttons[button_name].SetToolTip(tooltip)
+            self.buttons[button_name].Bind(wx.EVT_BUTTON, self._bindings[button_name])
+        for button_name in ['home', 'prevscr', 'prev']:
+            sizer.Add(self.buttons[button_name], 0, wx.ALIGN_CENTER | wx.EXPAND) 
+        self.inner = wx.lib.scrolledpanel.ScrolledPanel(self)
+        sizer.Add(self.inner, 1, wx.EXPAND | wx.ALL, 1)
+        for button_name in ['next', 'nextscr', 'end']:
+            sizer.Add(self.buttons[button_name], 0, wx.ALIGN_CENTER | wx.EXPAND)
+
         self.SetSizer(sizer, True)
-        return sizer
+        self.currently_selected_subpanel = None
+
+        
+    def next_root(self, event):
+        index = min(self.max_index, self.visible_index + 1)
+        self.compute_root_visibility(self.visible_index+1)
+
+    def next_scroll_roots(self, event):
+        index = min(self.max_index, self.visible_index + len(self.visible_panels))
+        self.compute_root_visibility(index)
+
+    def prev_root(self, event):
+        index = max(0, self.visible_index - 1)
+        self.compute_root_visibility(index)
+
+    def prev_scroll_roots(self, event):
+        index = max(0, self.visible_index - len(self.visible_panels))
+        self.compute_root_visibility(index)
+
+    def last_root(self, event):
+        self.compute_root_visibility(self.max_index)
+        
+    def first_root(self, event):
+        self.compute_root_visibility(0)
+        
+    def compute_root_visibility(self, index):
+        inner_width = self.inner.GetSize()[0]
+        visible_panels_width = 0
+        for panel in self.visible_panels:
+            panel.Hide()
+        self.visible_panels = []
+        for root_panel in self.root_panels[index:]:
+            root_panel.Show()
+            visible_panels_width += root_panel.width
+            self.visible_panels.append(root_panel)
+            if visible_panels_width >= inner_width:
+                break
+        self.visible_index = index
+        self.inner.Layout()
+        self.inner.Refresh()
+        self.inner.Update()
+    
+    def regenerate(self):
+        self.currently_selected_subpanel = None
+        self.visible_index = 0
+        self.max_index = len(self.data.sorted_hierarchies(self.sorting_method)) - 1
+        self.inner_sizer = sizer = wx.BoxSizer(wx.HORIZONTAL)
+        for panel in self.root_panels:
+            panel.Destroy()
+        
+        self.root_panels = []
+        self.visible_panels = []
+        for hierarchy_root in self.data.sorted_hierarchies(self.sorting_method):
+            panel = RootPanel(parent = self.inner, shape = hierarchy_root, main_widget = self)
+            self.root_panels.append(panel)
+            sizer.Add(panel, 0, wx.ALIGN_TOP | wx.EXPAND)
+            panel.Hide()
+        sizer.AddStretchSpacer()
+        self.inner.SetSizer(sizer, True)
+        self.inner.SetupScrolling(scroll_x = False)
+        self.compute_root_visibility(self.visible_index)
 
     def select(self, shape):
         for panel in self.root_panels:
@@ -113,10 +184,7 @@ class RootsPanel(wx.lib.scrolledpanel.ScrolledPanel):
                 panel.select()
                 break
     
-    
-    
     def set_hierarchy_sorting_method(self, sorting_method = None):
         if (sorting_method != self.sorting_method):
             self.sorting_method = sorting_method
             self.regenerate()    
-
